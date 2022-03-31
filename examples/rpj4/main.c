70e5519f22eb79cfd81218826e394bc0051d35bd
bferguson3 <ferguson.ben86@gmail.com>
2022-03-31 16:20:26 -0400

    RPJ4 progress

--- examples/rpj4/main.c
+++ examples/rpj4/main.c
@@ -4,6 +4,46 @@
 #include "sprites.h"
 
 #include "graphics.h"
+#include "gfx/deck.h"
+#include "gfx/roboboar.h"
+
+// TRAP EXAMPLES
+// string descriptor 
+// what stats it targets, tgt val
+// how much damage it will do 
+
+const char desctest[256] = \
+"You rummage about for a bit, but find nothing useful.\n\
+Just then, a wild rampaging RoboBoar crashes through the brush! \n\
+Will you 1) try to pull your gun and shoot it, or 2) abscond while\n\
+you are still able?";
+
+typedef struct encounter { 
+    const char* desc;
+    u8 stats[2];    // 0 1 or 2 to identify stat
+    u8 difficulty;  // 3-18
+    u8 damage;      // always on fail 
+} Encounter;
+
+// e.g. 
+
+// You encounter a raging pit bull!
+//  Do you:
+//  1. Try to shoot it 
+//  2. Run away 
+// Difficulty: 8
+
+// All stat checks are 2d6. 
+
+// Result: 4
+// Failed!
+//  Use effort? (Remaining: n)
+// 1 Yes   2 No (Fail)
+
+// NG! You take damage: 2 
+
+// OK! You got away safely. 
+
 
 
 const u8 map1[] = { \
@@ -23,9 +63,9 @@ u8 revealedMap[] = { \
     0,0,0,0,0,0,0,0,
     0,0,0,0,0,0,0,0,
     0,0,0,0,0,0,0,0,
-    2,2,0,0,0,0,0,0,
-    1,2,1,0,0,0,0,0,
-    1,2,0,0,0,0,0,0,
+    1,1,0,0,0,0,0,0,
+    1,1,1,0,0,0,0,0,
+    1,1,0,0,0,0,0,0,
     0,0,0,0,0,0,0,0,
     0,0,0,0,0,0,0,0
 };
@@ -57,7 +97,20 @@ u8* tilePtrs[8];
 char* res;
 s16 randnum;
 bool frameDone;
+bool playerMoved;
+XYPos player_pos;
+XYPos targetHex;
 //struct xypos zeropos;
+u8 lastKey;
+u8 inputMode;
+Encounter test;
+
+#define EXPLORING 1
+#define WAITING 0
+#define ENC_SELECT 2
+#define GUN 0
+#define SPEED 1
+#define BOOK 2
 
 // F DEFS
 void DrawHexTile(u8 x, u8 y, u8 index);
@@ -65,7 +118,14 @@ void DrawCleanGrid();
 s16 roll(u8 numDie, u8 sides, s8 mod);
 void GameInit();
 void DrawFullUI();
+void DrawAreaAroundPlayer();
+void DrawInputUI();
+void BufferInput();
+inline void MovementKeys();
 
+enum Inputs { 
+    UpRight=9, Right=6, DownRight=3, UpLeft=7, Left=4, DownLeft=1, Confirm=5, Cancel=0
+};
 
 void main()
 {
@@ -79,8 +139,16 @@ void main()
     playingSong = false;
     // set up quick draw ram
     
+    c = ReadIOReg(SYS_CTL_REGB);
+    if(c & V1MODE_FLAG)
+    {
+        SetCursorPos(0, 0);
+        print("V 2   M O D E   O N L Y ");
+        while(1){};
+    }
+
     GameInit();
-    
+
     // Set up currentSong pointers
     LoadSong(&song[0]);
     playingSong = true;
@@ -115,58 +183,41 @@ void main()
 // hero hex area ::
     player_hex_pos.x = 1;
     player_hex_pos.y = 4;
-    for(i = player_hex_pos.x-1; i <= player_hex_pos.x+1; i++)
-        DrawHexTile(i, player_hex_pos.y, map1[(player_hex_pos.y*8)+i]);
-    i -= 2;
-    if(player_hex_pos.y % 2 == 0) //even - go up and -1
-    {
-        DrawHexTile(i-1, player_hex_pos.y-1, map1[(player_hex_pos.y*8)+i-9]);
-        DrawHexTile(i, player_hex_pos.y-1, map1[(player_hex_pos.y*8)+i-8]);
-        // down and -1
-        DrawHexTile(i, player_hex_pos.y+1, map1[(player_hex_pos.y*8)+i-8]);
-        DrawHexTile(i-1, player_hex_pos.y+1, map1[(player_hex_pos.y*8)+i-7]);
-    } else { //odd - go up and +1
-        DrawHexTile(i+1, player_hex_pos.y-1, map1[(player_hex_pos.y*8)+i-7]);
-        DrawHexTile(i, player_hex_pos.y-1, map1[(player_hex_pos.y*8)+i-8]);
-        // d and
-        DrawHexTile(i, player_hex_pos.y+1, map1[(player_hex_pos.y*8)+i-8]);
-    }
+    DrawAreaAroundPlayer();
+
     // Hero:// MASKING TEST 
     // Remember, the sprites are 384 bytes of bitmap data, but they only take up
     //  96 bytes per plane. 
     // 0xfe80, 0xfee0, ff40, ffa0 room for 4
-    XYPos player_pos;
     player_pos.x = 12;
     player_pos.y = 64;
+    
+    // deck test 
+    DrawImage_V2(52, 162, &deck[0], 8, 38);
+    DrawImage_V2(67, 162, &deck[0], 8, 38);
+    
     // copy out mask
     ExpandedGVRAM_Copy_On();
     ALUCopyOut(GVRAM_BASE+(player_pos.y*80)+player_pos.x, TEMPGVR_SPRITE_1, 4, 24);
     // draw sprite 
     ExpandedGVRAM_On();
-    DrawTransparentImage_V2(player_pos.x, player_pos.y, &librarianSprite[0], 32/8, 24);
+    //DrawTransparentImage_V2(player_pos.x, player_pos.y, &librarianSprite[0], 32/8, 24);
+    
+    // encounter area 
+    DrawTransparentImage_V2(54, 152, &librarianSprite[0], 32/8, 24);
+    DrawTransparentImage_V2(69, 152, &roboboar[0], 32/8, 24);
     // return backup
     //ExpandedGVRAM_Copy_On();
     //ALUCopyIn(TEMPGVR_SPRITE_1, GVRAM_BASE+(player_pos.y*80)+player_pos.x, 4, 24);
-
-
+    
     // ALU off
     DisableALU(FASTMEM_OFF);
     ExpandedGVRAM_Off();   
-    // disable load text
-    SetCursorPos(75, 4);
-    print("2/2");
-    SetCursorPos(75, 6);
-    print("2/2");
-    SetCursorPos(75, 8);
-    print("2/2");
-    SetCursorPos(75, 12);
-    print("10/10");
-    SetCursorPos(30,19);
-    print("           ");
-    SetCursorPos(5,16);
-    print("[7   9]\n");
-    print("[4   6]  Move      [5]  Confirm\n");
-    print("[1   3]         [0]     Cancel");
+
+    DrawInputUI();
+    
+    inputMode = EXPLORING;
+    
     
     // First, Write the address of the Vblank routine to the CRTC IRQ vector @ f302
     SetCRTC_IRQ((void*)&Vblank);
@@ -174,21 +225,53 @@ void main()
     IRQ_ON              // run once!
     while(1)
     { 
-        // Main Loop
         // wait for 1/60 irq to finish
-        while(!frameDone){idleCount++;};
+        while(!frameDone)
+        {
+            idleCount++;
+            BufferInput();    
+        };
         IRQ_OFF 
         frameDone = false;
+            
+        if(lastKey != -1)
+        {
+            if(inputMode == EXPLORING)
+            {
+                if(lastKey == Confirm)
+                {
+                    // search this tile - 1 trap, 20 heal random
+                    // forest - 1-2 trap
+                }
+                else if (lastKey == Cancel)
+                {
+                    // open menu
+                }
+                else
+                { 
+                    MovementKeys();
+                    // now we have the target hex location 
+                    if(targetHex.x >= 0 && targetHex.x < 8)
+                        if(targetHex.y >= 0 && targetHex.y < 8) // make sure its on the map
+                        {
 
+                        }
+                    ;
+                    // if not valid tile, dont take a turn.
+                }
+            }
+        }
+        
+        // Main Loop
         // modify seed 
+        lastKey = -1;
         RANDOMSEED += rand();
-        randnum = roll(1, 6, 0);
+        randnum = (targetHex.x << 4) | targetHex.y;
         
         // return 
         IRQ_ON 
     }
 }
-
 
 void Vblank() __critical __interrupt
 {
@@ -202,6 +285,21 @@ void Vblank() __critical __interrupt
         //print("OVER!");
     }
 
+    if(playerMoved)
+    {
+        //copy spr to buffer 
+
+        // copy in old bg 
+
+        //get hex of direction moving
+        
+        // copy out bg buffer 
+
+        // copy in spr 
+
+        playerMoved = false;
+    }
+    
     // PRINT IDLE COUNTS
     //SetCursorPos(0, 0);
     //u8* d = byToHex((u8)(idleCount >> 8)); 
@@ -232,102 +330,4 @@ s16 roll(u8 numDie, u8 sides, s8 mod)
     return r;
 }
 
-void DrawHexTile(u8 x, u8 y, u8 index)
-{
-    
-    u8 z = y & 0b1;
-    y = y >> 1;
-    switch(z)
-    {
-        case(0):
-            DrawImage_V2(2+(x*8), (y*32)+8, tilePtrs[index], 64/8, 24);
-            break;
-        case(1):
-            DrawImage_V2(6+(x*8), (y*32)+24, tilePtrs[index], 64/8, 24);
-            break;
-        default:
-            break;
-    }
-}
-
-void DrawCleanGrid()
-{
-    DrawImage_V2(2, 8, &hexa[0], 64/8, 24);
-    SetIOReg(EXPANDED_GVRAM_CTRL, (u8)(0x80 | bit(4) ));
-    for(u8 x = 0; x < 8; x++)
-    {
-        ALUCopy(VRAMAddrByTile(2,8), VRAMAddrByTile(2+(8*x),8), 8, 24);
-        ALUCopy(VRAMAddrByTile(2,8), VRAMAddrByTile(2+(8*x),40), 8, 24);
-        ALUCopy(VRAMAddrByTile(2,8), VRAMAddrByTile(2+(8*x),72), 8, 24);
-        ALUCopy(VRAMAddrByTile(2,8), VRAMAddrByTile(2+(8*x),104), 8, 24);
-    }
-    ALUCopy(VRAMAddrByTile(2,8), VRAMAddrByTile(6,24), 8, 24);
-    for(u8 x = 0; x < 8; x++)
-    {
-        ALUCopy(VRAMAddrByTile(6,24), VRAMAddrByTile(6+(8*x),24), 8, 24);
-        ALUCopy(VRAMAddrByTile(6,24), VRAMAddrByTile(6+(8*x),56), 8, 24);
-        ALUCopy(VRAMAddrByTile(6,24), VRAMAddrByTile(6+(8*x),88), 8, 24);
-        ALUCopy(VRAMAddrByTile(6,24), VRAMAddrByTile(6+(8*x),120), 8, 24);
-    }
-    //*********//
-    SetIOReg(EXPANDED_GVRAM_CTRL, (u8)(0x80));
-
-}
-
-void GameInit()
-{
-    zeropos.x = 0;
-    zeropos.y = 0;
-    player_hex_pos.x = 0;
-    player_hex_pos.y = 0;
-    for(u8 i = 0; i < 8; i++ )
-    {
-        loadedTiles[i].pos = zeropos; // original tile to copy
-        loadedTiles[i].tileNo = 0;    // number to pull from tilePtrs[]
-    }
-    tilePtrs[0] = &hexa[0];
-    tilePtrs[1] = &grasstile[0];
-    tilePtrs[2] = &treetile[0];
-}
-
-void DrawFullUI()
-{
-    u8 i;   
-    // PAPER ROLL UI THING
-    DrawImage_V2(0, 0, &paperroll[0], 2, 8);
-    
-    DrawImage_V2(0, 8, &paperside[0], 2, 36);
-    DrawImage_V2(0, 44, &paperside[0], 2, 36);
-    DrawImage_V2(0, 80, &paperside[0], 2, 36);
-    DrawImage_V2(0, 116, &paperside[0], 2, 36);
-    
-    DrawImage_V2(70, 8, &paperside[0], 2, 36);
-    DrawImage_V2(70, 44, &paperside[0], 2, 36);
-    DrawImage_V2(70, 80, &paperside[0], 2, 36);
-    DrawImage_V2(70, 116, &paperside[0], 2, 36);
-    
-    for(i = 2; i < 13; i++)
-        DrawImage_V2(i, 1, &rolltop[0], 1, 6);
-    SetIOReg(EXPANDED_GVRAM_CTRL, (u8)(0x80 | bit(4) ));
-    for(i = 10; i < 64; i+=8)
-        ALUCopy(VRAMAddrByTile(2,1),VRAMAddrByTile(3+i,1), 8, 6);
-    ExpandedGVRAM_On();    
-    DrawImage_V2(71, 1, &paperend[0], 1, 6);    
-    DrawImage_V2(1, 149, &paperbottom[0], 2, 2);
-    DrawImage_V2(3, 149, &paperbottom[0], 2, 2);
-    DrawImage_V2(5, 149, &paperbottom[0], 2, 2);
-    DrawImage_V2(7, 149, &paperbottom[0], 2, 2);
-    SetIOReg(EXPANDED_GVRAM_CTRL, (u8)(0x80 | bit(4) ));
-    for(i = 8; i < 64; i+=8)
-        ALUCopy(VRAMAddrByTile(1,149),VRAMAddrByTile(i,149), 8, 2);
-    ExpandedGVRAM_On();
-    //////
-    
-    // LIBBY UI
-    DrawImage_V2(72, 8,     &libbytext[0],  7, 14);
-    DrawImage_V2(73, 32,    &gunicon[0],    2, 7);
-    DrawImage_V2(73, 51,    &booticon[0],   2, 8);
-    DrawImage_V2(73, 69,    &bookicon[0],   2, 11);
-    DrawImage_V2(72, 116,   &hearticon[0],  3, 7);
-    //
-}
+#include "rpj4.c"
